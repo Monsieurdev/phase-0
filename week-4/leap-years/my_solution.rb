b1299696cafcc05670b69593019b17f1126958a6
Mohamed Monekata <mameloga@hotmail.fr>
2016-03-03 00:32:56 -0500

    Add exercises for pairing

---
+++ week-4/leap-years/my_solution.rb
@@ -0,0 +1,20 @@
+# Leap Years
+
+# I worked on this challenge  with Benjamin Flores
+
+
+# Your Solution Below
+def leap_year?(year)
+if (year % 4 == 0 ) && !(year % 100 == 0)
+	return true
+elsif (year % 400 == 0)
+	return true
+elsif (year % 4 == 0) && (year % 100 == 0) && !(year % 400 ==0)
+	return false
+
+elsif !(year % 4 ==0)
+	return false
+	
+end
+end
+
